26096a6db456b83217a686159843a736f05ee94b
pycrab <pycrab@192.168.52.1>
2018-08-27 12:29:40 +0800

    桶排序

---
+++ src/com/sortalgerithm/BucketSort.java
@@ -0,0 +1,38 @@
+package com.sortalgerithm;
+/** 
+* @author: pycrab
+* @Date: 2018年8月27日 下午12:05:23 
+*/
+public class BucketSort {
+
+	private int[] arr;
+	
+	public BucketSort() {
+		arr = new int[10];
+		for (int i = 0; i < arr.length; i++) {
+			arr[i] = (int) (Math.random() * 10);
+		}
+	}
+	
+	public void bucketSort() {
+		
+		// 创建桶
+		int[] buckets = new int[11];
+		for (int i = 0; i < arr.length; i++) {
+			buckets[arr[i]] += 1;
+		}
+		
+		for (int i = 0; i < buckets.length; i++) {
+			for (int j = 0; j < buckets[i]; j++) {
+				System.out.println(i + " ");
+			}
+		}
+	}
+	
+	public static void main(String[] args) {
+		BucketSort bucket = new BucketSort();
+		bucket.bucketSort();
+
+	}
+
+}
