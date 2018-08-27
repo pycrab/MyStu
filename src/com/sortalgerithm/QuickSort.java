f09decb81741ea79debd2e9f2e1ae2161777d1ea
pycrab <pycrab@192.168.52.1>
2018-08-27 21:21:34 +0800

    快速排序

--- src/com/sortalgerithm/QuickSort.java
+++ src/com/sortalgerithm/QuickSort.java
@@ -22,12 +22,13 @@ public class QuickSort {
 		if(left > right) {
 			return;
 		}
-		// 取第一个数作为基准数，i和j为游标
+		// 1、取第一个数作为基准数，i和j为左右游标
 		int temp = arr[left];
 		int i = left;
 		int j = right;
 		
 		while (i != j) {
+			// 2、左右游标扫描
 			// 先判断右边
 			while (arr[j] >= temp && i < j) {
 				j--;
@@ -36,15 +37,15 @@ public class QuickSort {
 			while (arr[i] <= temp && i < j) {
 				i++;
 			}
-			
-			// 交换两个数的位置
+
+			// 3、左右游标相遇，交换两个数的位置
 			if(i < j) {
 				int t = arr[i];
 				arr[i] = arr[j];
 				arr[j] = t;
 			}
 		}
-		// 将基准数归位
+		// 4、将基准数归位
 		arr[left] = arr[i];
 		arr[i] = temp;
 		
@@ -52,6 +53,16 @@ public class QuickSort {
 		quickSort(left, i-1);
 		quickSort(i+1, right);
 	}
+	
+	/**
+	 * 快速排序优化点：
+	 * 	1. 对小数组进行插入排序，而不进行递归
+	 * 		修改 if(left + M >= right){InsertSort(left, right); return;}
+	 *  2. 随机选取基准数并与left位置的数交换
+	 *  	修改 int ranIndex = (int)(left+Math.random()*(right-left));exchange(arr, left, ranIndex);
+	 *      或者三数取中法，选择首位和中间的数的中位数作为基准数
+	 * 
+	 */
 	
 	public static void main(String[] args) {
 		QuickSort quick = new QuickSort();
