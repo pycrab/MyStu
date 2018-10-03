e1c4dd7a7a393682a59eaf2a88d1d7d261bd491f
pycrab <pycrab@pycrab>
2018-10-03 17:51:42 +0800

    多线程基础

--- src/com/thread/ThreadDemo1.java
+++ src/com/thread/ThreadDemo1.java
@@ -1,30 +1,17 @@
 package com.thread;
 
+import java.util.concurrent.Callable;
+import java.util.concurrent.FutureTask;
+
 /**
- *实现多线程的两种方式：
- *1.继承Thread类
- *2.实现Runnable接口（推荐）
- *
- *线程睡眠sleep方法和线程让步yeild方法
- *	两者相似
- *	sleep方法可以设置线程暂停时间，同时让出CPU，进入不可运行状态，
- *		让其他线程有机会执行，但是不释放锁，即其他线程不能访问共享数据
- *	yeild方法也是使线程暂停，同时让出CPU，但是它仍处于可运行状态，
- *		它是让相同优先级的线程有机会执行，如果检测没有同优先级的线程，它会继续执行原来的线程，它也不释放锁
- *
- *线程同步join方法
- *	join方法是暂停当前线程的执行，而等待指定线程执行完毕或者执行一定时间
- *
- *	参数可选填时间，表示指定线程执行时间
- *	必须在start之后使用才有意义
- *	join使线程的并发执行变成串行执行
- *	join使用了线程的wait方法来实现同步，如果A线程内调用B线程的join，就是A当线程调用了B线程的wait方法，A线程进入阻塞状态，直到B线程执行完
- *
  * @author pycrab
  *
  */
 public class ThreadDemo1 {
 
+	/**
+	 * 方式一
+	 */
 	static class MyThread extends Thread{
 		//需要重写run方法
 		@Override
@@ -41,6 +28,10 @@ public class ThreadDemo1 {
 			}
 		}
 	}
+	
+	/**
+	 * 方式二
+	 */
 	static class MyRunnable implements Runnable{
 		//需要重写run方法
 		@Override
@@ -58,35 +49,45 @@ public class ThreadDemo1 {
 		}
 	}
 	
+	/**
+	 * 方式三
+	 */
+	static class MyCallable implements Callable<Integer>{
+
+		@Override
+		public Integer call() throws Exception {
+			System.out.println("Implement Callable");
+			return 1;
+		}
+		
+	}
+	
 	public static void main(String[] args) {
 		
 		//报错No enclosing instance of type ThreadDemo is accessible. Must qualify the allocation with an enclosing instance of type ThreadDemo (e.g. x.new A() where x is an instance of ThreadDemo).
 		//需要使用静态内部类
+		
 		//1.创建线程对象
 		MyThread tr1 = new MyThread();
-		
-		//2.当前线程是否处于活动状态
+		//2.启动线程，进入就绪队列
+		tr1.start();
+		//3.当前线程是否处于活动状态
 		System.out.println("isAlive=" + tr1.isAlive());
-		
-		//3.设置线程优先级，只是改变几率
+		//4.设置线程优先级，只是改变几率
 //		tr1.setPriority(Thread.MAX_PRIORITY);
-		
-		//4.设置线程为守护线程
+		//5.设置线程为守护线程
 		//（Java线程分为用户线程和守护线程，一个前台一个后台）
 		//当只有一个守护线程，用户线程都已退出运行时，JVM就会结束
 		//设置守护线程必须在启动之前，守护线程中产生的新线程也是守护线程，不是所有的应用都可以分配守护线程为之服务，如读写操作或计算逻辑
 //		tr1.setDaemon(true);
 		
-		//5.启动线程，进入就绪队列
-		tr1.start();
-		System.out.println("isAlive=" + tr1.isAlive());
-
-		
 		MyRunnable tr = new MyRunnable();
-
 		Thread tr2 = new Thread(tr);
 		tr2.start();
 
+		MyCallable mc = new MyCallable();
+		FutureTask<Integer> fTask = new FutureTask<>(mc);
+		new Thread(fTask).start();
 
 		for(int i = 0;i < 10; ++i) {
 			System.out.println(i + "-" + Thread.currentThread().getName() + "--" + Thread.currentThread().getPriority());
